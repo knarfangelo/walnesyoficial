9b609a8108a3fdd9271b0950c32772b52216ad99
knarfangelo <knarf2003angelo@gmail.com>
2024-08-12 15:57:07 -0500

    Walnesy 1 parte terminado

--- src/app/components/conoceme/conoceme.component.ts
+++ src/app/components/conoceme/conoceme.component.ts
@@ -13,16 +13,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         CONÓCEME
       </h1>
       <main>
-        <div class="empresario">
+        <a href="https://grupoalcanza.com/" target="_blank" class="empresario">
         <div> 
         <img class="alcanza" src="/conoceme/alcanza.png" alt=""><p>EMPRESARIO</p></div> 
           <img src="/conoceme/walnesy-empresario.png" alt="">
-        </div>
-        <div class="politico">
+</a>
+        <a href="https://fuerzadelpueblo.do/" target="_blank" class="politico">
           <div>
           <img class="logo" src="/conoceme/fuerzadelpueblo.png" alt=""><p>POLÍTICO</p></div>
           <img class="walnesy" src="/conoceme/walnesy-politico.png" alt="">
-        </div>
+</a>
       </main>
 
   </header>
