e05800fb3068422a088188736622eec1ec1c670d
knarfangelo <knarf2003angelo@gmail.com>
2024-08-14 15:25:10 -0500

    los links de las redes sociales acortados

--- src/app/components/navegacion/navegacion.component.ts
+++ src/app/components/navegacion/navegacion.component.ts
@@ -20,7 +20,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       <ul class="icons">
         <li><a href="https://www.instagram.com/walnesyborquez/"target="_blank"><img src="/icons/instagram.png" alt="instagram de walnesy"></a></li>
         <li><a href=""><img src="/icons/tiktok.png" alt="tiktok de walnesy"></a></li>
-        <li><a href="https://www.facebook.com/share/YJFeZJQCcqyWWj5y/?mibextid=qi2Omg" target="_blank"><img src="/icons/facebook.png" alt="facebook de walnesy"></a></li>  
+        <li><a href="https://www.facebook.com/walnesy" target="_blank"><img src="/icons/facebook.png" alt="facebook de walnesy"></a></li>  
         <li><a href="https://x.com/walnesyborquez" target="_blank"><img src="/icons/twitter.png" alt="twitter de walnesy"></a></li>
         <li><a href="https://www.youtube.com/@walnesyborquez5071" target="_blank"><img src="/icons/youtube.png" alt="youtube de walnesy" target="_blank"></a></li>
       </ul>
@@ -30,5 +30,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavegacionComponent { 
-  
+
 }
