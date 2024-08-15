7e0e19ff312bb6ab9628a8e51288cba34d6dc379
knarfangelo <knarf2003angelo@gmail.com>
2024-08-15 15:45:54 -0500

    Navegacion terminado

--- src/app/components/navegacion/nav-responsive/nav-responsive.component.ts
+++ src/app/components/navegacion/nav-responsive/nav-responsive.component.ts
@@ -9,8 +9,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   ],
   template: `
   <div class="relleno"></div>
-  <header>
-     <nav>
+  <nav>
       <ul class="icons">
         <li><button (click)="navegacion()" class="burguer"><img class="burguer" src="icons/burguer-walnesy.svg" alt=""></button></li>
         <li><a href="https://www.instagram.com/walnesyborquez/"target="_blank"><img src="/icons/instagram.png" alt="instagram de walnesy"></a></li>
@@ -19,21 +18,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         <li><a href="https://x.com/walnesyborquez" target="_blank"><img src="/icons/twitter.png" alt="twitter de walnesy"></a></li>
         <li><a href="https://www.youtube.com/@walnesyborquez5071" target="_blank"><img src="/icons/youtube.png" alt="youtube de walnesy" target="_blank"></a></li>
       </ul>
-    </nav><div class="background">
-          </div> 
-          <main  [class.open]="nav">
-         
+    </nav>
+  <header>      
+    <main  [class.open]="nav">
       <ul class="navegacion">
         <li><button (click)="navegar()"><img src="icons/close.svg" alt=""></button></li>
         <li><img class="logo" src="banner/logo.png" alt=""></li>
-        <li class="lista"><a href="#inicio">INICIO</a></li>
-        <li class="lista"><a href="#conoceme">CONÓCEME</a></li>
-        <li class="lista"><a href="#blog">BLOG</a></li>
-        <li class="lista"><a href="#secomp">SECOMP</a></li>
-        <li class="lista"><a href="#voluntariado">VOLUNTARIADO</a></li>
+        <li class="lista"><a (click)="navegar()" href="#inicio">INICIO</a></li>
+        <li class="lista"><a (click)="navegar()" href="#conoceme">CONÓCEME</a></li>
+        <li class="lista"><a (click)="navegar()" href="#blog">BLOG</a></li>
+        <li class="lista"><a (click)="navegar()" href="#secomp">SECOMP</a></li>
+        <li class="lista"><a (click)="navegar()" href="#voluntariado">VOLUNTARIADO</a></li>
         <li><div class="separador"></div></li>
         <li class="noticias"><a href="" class="noticias-p" >NOTICIAS</a></li>
       </ul>
+      <div class="background"></div>
       </main>
     </header>
   `,
@@ -43,10 +42,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 export class NavResponsiveComponent { 
 
   nav = false;
-  background = false;
   navegacion() {
     this.nav = !this.nav;
-    this.background = !this.background;
   }
   navegar(){
     this.nav = false;
